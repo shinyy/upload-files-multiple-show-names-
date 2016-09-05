916863b9d4a78d857968eee636c1400d54ab21b7
shinyy <252865778@qq.com>
2016-09-05 15:37:39 +0800

    改

--- js/js.js
+++ js/js.js
@@ -5,14 +5,14 @@ $(function(){
 			var obj=document.getElementsByClassName("fileBtn")[0];
 			var temp="";
 			for(var i=0;i<obj.files.length;i++){
-				temp=temp+"<p>"+obj.files[i].name+"</p>";
+				temp=temp+"<p>"+obj.files[i].name+"<span class='uploadState'>等待上传······</span></p>";
 				}
 			$(".filesName").html(temp);
-			if(temp!=""){
+			/*if(temp!=""){
 				$(".uploadState").show();
 				}else{
 					$(".uploadState").hide();
-					}
+					}*/
 			};
 			
 		$(".fileBtn").change(function(){
